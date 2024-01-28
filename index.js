2b5c68595c1bf840070c841a852a1c29afe44506
MohammedHassan07 <mhassancs260@gmail.com>
2024-01-28 07:56:20 -0800

    feat - Added the scroll reveal

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ btnUpArrow.addEventListener('click', (event) => {
 
     document.documentElement.scrollTop = 0
 })
+
+// scroll reveal
+ScrollReveal({
+
+    reset: true,
+    distance: '80px',
+    duration: '2000',
+    delay: '200'
+
+})
+ScrollReveal().reveal('.my-intro', {origin: 'top'});
+ScrollReveal().reveal('.my-img', {origin: 'bottom'});
+ScrollReveal().reveal('.logo-img', {origin: 'left'});
+ScrollReveal().reveal('.project-description', {origin: 'right'});
+ScrollReveal().reveal('.my-img-2', {origin: 'bottom'});
+ScrollReveal().reveal('.my-contact', {origin: 'top'});
+ScrollReveal().reveal('.my-footer', {origin: 'left'});
+ScrollReveal().reveal('.my-scroll-btn', {origin: 'right'});
